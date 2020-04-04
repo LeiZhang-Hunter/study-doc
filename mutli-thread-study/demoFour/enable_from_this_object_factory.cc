a968d800d8fcfa92458fc261197d32e8b2bccada
root <357733652@qq.com>
2020-04-04 19:07:58 +0800

    关于c++多线程服务器编程的学习

---
+++ mutli-thread-study/demoFour/enable_from_this_object_factory.cc
@@ -0,0 +1,118 @@
+#include <memory>
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <pthread.h>
+#include <map>
+#include <functional>
+using namespace std;
+using namespace std::placeholders;
+class MutexLock
+{
+public:
+    MutexLock()
+    {
+        pthread_mutex_init(&_mutex, NULL);
+    }
+
+    ~MutexLock()
+    {
+        pthread_mutex_destroy(&_mutex);
+    }
+
+    void lock()
+    {
+        pthread_mutex_lock(&_mutex);
+    }
+
+    void unlock()
+    {
+        pthread_mutex_unlock(&_mutex);
+    }
+
+    pthread_mutex_t * getMutexLockPtr()
+    {
+        return &_mutex;
+    }
+private:
+    pthread_mutex_t _mutex;
+};
+
+//RAII
+class MutexLockGuard
+{
+public:
+    MutexLockGuard(MutexLock & mutex)
+            : _mutex(mutex)
+    {
+        _mutex.lock();
+    }
+
+    ~MutexLockGuard()
+    {
+        _mutex.unlock();
+    }
+
+private:
+    MutexLock & _mutex;
+};
+
+class Stock{
+public:
+    Stock(const string& key)
+    {
+        keyData = key;
+    }
+
+    const string& key()
+    {
+        return keyData;
+    }
+
+private:
+    string keyData;
+};
+
+class StockFactory :public enable_shared_from_this<StockFactory>
+{
+public:
+    shared_ptr<Stock> get(const string& key)
+    {
+        shared_ptr<Stock> pStock;
+        MutexLockGuard guard(mutex_);
+        weak_ptr<Stock> wkStock = stocks[key];
+        pStock = wkStock.lock();
+        if(!pStock){
+            pStock.reset(new Stock(key),std::bind(&StockFactory::deleteStock
+                    ,shared_from_this(),_1));
+            wkStock = pStock;
+        }
+        return pStock;
+    }
+
+private:
+    mutable MutexLock mutex_;
+    map<string,shared_ptr<Stock>> stocks;
+
+    void deleteStock(Stock* stock)
+    {
+        if (stock) {
+            MutexLockGuard lock(mutex_);
+            stocks.erase(stock->key());
+        }
+        delete stock; // sorry, I lied
+    }
+};
+shared_ptr<StockFactory> factory(new StockFactory);
+
+void  test()
+{
+    shared_ptr<Stock> a = factory->get("ahahah");
+}
+
+int main()
+{
+    test();
+    return  0;
+
+}
---
+++ mutli-thread-study/demoFour/readme.md
@@ -0,0 +1,17 @@
+# 内容介绍
+
+这篇笔记学习自陈硕先生的 c++ 多线程服务器编程
+
+
+上一节中bind 传入 this指针，暗藏风险，因为StockFactory::get() 把原始指针 this 保存到了 boost::function，如 果 StockFactory 的 生 命 期 比 Stock 短, 那 么 Stock 析 构 时 去 回 调 StockFactory::deleteStock 就会 core dump。因为你无法知道这个指针是否存活，所以我们可以enable_shared_from_this，
+所以我们使用boost::enable_shared_from_this<StockFactory>,把this指针变成shared_ptr传进去，当然申请方式由之前的
+
+```
+StockFactory factory;
+```
+
+变为了
+
+```
+shared_ptr<StockFactory> factory(new StockFactory);
+```
